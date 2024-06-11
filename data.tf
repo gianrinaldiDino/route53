521dd3471d9677347fa49320995988cdd0770e5d
Gian Rinaldi <gian.rinaldi@dinocloudconsulting.com>
2024-06-10 23:06:55 -0300

    secon dcommit.

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    region   = var.region
-    bucket   = "dinosandbox-prod-tf-states-us-east-1"
-    key      = "production/us-east-1/vpc/terraform.tfstate"
-  }
-}
